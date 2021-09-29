8186df51149c5c9e22c2cb9a059861316b11a78f
Sammwy <sammwy.dev@gmail.com>
2021-09-29 02:17:23 -0300

    Fixed cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
-import path from "path";
-
 import router from "./routes/router";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://mcstats.live",
+  })
+);
 app.use(morgan("dev"));
 app.use(router);
 
